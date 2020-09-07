c5ee52abbca33b4a725762bfbbae470f0870711a
Daniel Bauer <daniel.bauer@softaware.at>
2020-09-07 13:26:07 +0200

    BugFix: Invalid json file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 /scc $@ -f json -o scc-result.json
 
 # Trim json file to only contain relevant data
-cat scc-result.json | /jq '.[] | { name: .Name, lines: .Lines, code: .Code, comment: .Comment, blank: .Blank, files: .Count }' > healthcheck-loc.json
+cat scc-result.json | /jq 'map({ name: .Name, lines: .Lines, code: .Code, comment: .Comment, blank: .Blank, files: .Count })' > healthcheck-loc.json
 chmod 777 healthcheck-loc.json
 
 # Output result to console
